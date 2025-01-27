fed8f9d3ba33462ba7ecb939b37014581f6967b9
Wout Neirynck <wout@monkey-projects.be>
2025-01-27 13:48:08 +0100

    Fix curl cmd

--- .monkeyci/build_test.clj
+++ .monkeyci/build_test.clj
@@ -1,5 +1,6 @@
 (ns build-test
   (:require [clojure.test :refer [deftest testing is]]
+            [clojure.string :as cs]
             [build :as sut]
             [monkey.ci.build.v2 :as m]))
 
@@ -8,5 +9,11 @@
     (is (m/container-job? sut/metrics-plugin))))
 
 (deftest prepare-image
-  (testing "provides container job"
-    (is (m/container-job? sut/prepare-image))))
+  (let [job sut/prepare-image]
+    (testing "provides container job"
+      (is (m/container-job? job)))
+
+    (testing "downloads artemis sources"
+      (let [cmd (-> job :script first)]
+        (is (cs/starts-with? cmd "curl"))
+        (is (cs/ends-with? cmd " -o artemis-src.tgz"))))))
